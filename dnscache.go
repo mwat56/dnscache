f4ea0e023280a9ab57d8dda81db4c4c85faac617
Matthias Watermann <support@mwat.de>
2025-05-12 14:04:31 +0200

    + initial checkin;

--- dnscache.go
+++ dnscache.go
@@ -1,205 +1,77 @@
-/*
-   Copyright © 2025  M.Watermann, 10247 Berlin, Germany
-               All rights reserved
-           EMail : <support@mwat.de>
-*/
-package main
-
-//lint:file-ignore ST1017 - I prefer Yoda conditions
+package dnscache
+// Package dnscache caches DNS lookups
 
 import (
-	"context"
-	"crypto/tls"
-	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"net/url"
-	"os"
-	"os/signal"
-	"path/filepath"
-	"runtime"
-	"syscall"
-	"time"
-
-	"github.com/NYTimes/gziphandler"
-	"github.com/mwat56/apachelogger"
-	"github.com/mwat56/errorhandler"
-	"github.com/mwat56/dnscache"
+  "net"
+  "sync"
+  "time"
 )
 
-// `exit()` Log `aMessage` and terminate the program.
-func exit(aMessage string) {
-	apachelogger.Err("APPNAME/main", aMessage)
-	runtime.Gosched() // let the logger write
-	log.Fatalln(aMessage)
-} // exit()
+type Resolver struct {
+  lock sync.RWMutex
+  cache map[string][]net.IP
+}
 
-// `redirHTTP()` Send HTTP clients to HTTPS server.
-//
-// see: https://gist.github.com/d-schmidt/587ceec34ce1334a5e60
-func redirHTTP(aWriter http.ResponseWriter, aRequest *http.Request) {
-	// Copy the original URL and replace the scheme:
-	targetURL := url.URL{
-		Scheme:     `https`,
-		Opaque:     aRequest.URL.Opaque,
-		User:       aRequest.URL.User,
-		Host:       aRequest.URL.Host,
-		Path:       aRequest.URL.Path,
-		RawPath:    aRequest.URL.RawPath,
-		ForceQuery: aRequest.URL.ForceQuery,
-		RawQuery:   aRequest.URL.RawQuery,
-		Fragment:   aRequest.URL.Fragment,
-	}
-	target := targetURL.String()
+func New(refreshRate time.Duration) *Resolver {
+  resolver := &Resolver {
+    cache: make(map[string][]net.IP, 64),
+  }
+  if refreshRate > 0 {
+    go resolver.autoRefresh(refreshRate)
+  }
+  return resolver
+}
 
-	apachelogger.Err(`APPNAME/main`, `redirecting to: `+target)
-	http.Redirect(aWriter, aRequest, target, http.StatusTemporaryRedirect)
-} // redirHTTP()
-)
+func (r *Resolver) Fetch(address string) ([]net.IP, error) {
+  r.lock.RLock()
+  ips, exists := r.cache[address]
+  r.lock.RUnlock()
+  if exists { return ips, nil }
 
-// `setupSignals()` configures the capture of the interrupts `SIGINT`
-// `and `SIGTERM` to terminate the program gracefully.
-func setupSignals(aServer *http.Server) {
-	// handle `CTRL-C` and `kill(15)`.
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+  return r.Lookup(address)
+}
 
-	go func() {
-		for signal := range c {
-			msg := fmt.Sprintf("%s captured '%v', stopping program and exiting ...", filepath.Base(os.Args[0]), signal)
-			apachelogger.Err(`Nele/catchSignals`, msg)
-			log.Println(msg)
-			break
-		} // for
+func (r *Resolver) FetchOne(address string) (net.IP, error) {
+  ips, err := r.Fetch(address)
+  if err != nil || len(ips) == 0 { return nil, err}
+  return ips[0], nil
+}
 
-		ctx, cancel := context.WithCancel(context.Background())
-		aServer.BaseContext = func(net.Listener) context.Context {
-			return ctx
-		}
-		aServer.RegisterOnShutdown(cancel)
+func (r *Resolver) FetchOneString(address string) (string, error) {
+  ip, err := r.FetchOne(address)
+  if err != nil || ip == nil { return "", err }
+  return ip.String(), nil
+}
 
-		ctxTimeout, cancelTimeout := context.WithTimeout(
-			context.Background(), time.Second*10)
-		defer cancelTimeout()
-		if err := aServer.Shutdown(ctxTimeout); nil != err {
-			exit(fmt.Sprintf("%s: %v", filepath.Base(os.Args[0]), err))
-		}
-	}()
-} // setupSignals()
+func (r *Resolver) Refresh() {
+  i := 0
+  r.lock.RLock()
+  addresses := make([]string, len(r.cache))
+  for key, _ := range r.cache {
+    addresses[i] = key
+    i++
+  }
+  r.lock.RUnlock()
 
+  for _, address := range addresses {
+    r.Lookup(address)
+    time.Sleep(time.Second * 2)
+  }
+}
 
-// Actually run the program …
-func main() {
-	var (
-		err     error
-		handler http.Handler
-		ph      *nele.TPageHandler
-	)
-	Me, _ := filepath.Abs(os.Args[0])
+func (r *Resolver) Lookup(address string) ([]net.IP, error) {
+  ips, err := net.LookupIP(address)
+  if err != nil { return nil, err }
 
-	// Read INI files and commandline options
-	APPNAME.InitConfig()
+  r.lock.Lock()
+  r.cache[address] = ips
+  r.lock.Unlock()
+  return ips, nil
+}
 
-	if ph, err = nele.NewPageHandler(); nil != err {
-		nele.ShowHelp()
-		exit(fmt.Sprintf("%s: %v", Me, err))
-	}
-
-	// Setup the errorpage handler:
-	handler = errorhandler.Wrap(ph, ph)
-
-	// Inspect `gzip` commandline argument and setup the Gzip handler:
-	if APPNAME.AppArgs.GZip {
-		handler = gziphandler.GzipHandler(handler)
-	}
-
-	// Inspect logging commandline arguments and setup the `ApacheLogger`:
-	handler = apachelogger.Wrap(handler, nele.AppArgs.AccessLog, nele.AppArgs.ErrorLog)
-
-	ctxTimeout, cancelTimeout := context.WithTimeout(
-		context.Background(), time.Second*10)
-	defer cancelTimeout()
-
-	// We need a `server` reference to use it in `setupSignals()`
-	// and to set some reasonable timeouts:
-	server := &http.Server{
-		// The TCP address for the server to listen on:
-		Addr: APPNAME.AppArgs.Addr,
-		// Return the base context for incoming requests on this server:
-		BaseContext: func(net.Listener) context.Context {
-			return ctxTimeout
-		},
-		// Request handler to invoke:
-		Handler: handler,
-		// Set timeouts so that a slow or malicious client
-		// doesn't hold resources forever
-		//
-		// The maximum amount of time to wait for the next request;
-		// if IdleTimeout is zero, the value of ReadTimeout is used:
-		IdleTimeout: 0,
-		// The amount of time allowed to read request headers:
-		ReadHeaderTimeout: 10 * time.Second,
-		// The maximum duration for reading the entire request,
-		// including the body:
-		ReadTimeout: 10 * time.Second,
-		// The maximum duration before timing out writes of the response:
-		// WriteTimeout: 10 * time.Second,
-		WriteTimeout: -1, // see whether this eliminates "i/o timeout HTTP/1.0"
-	}
-	if 0 < len(APPMAME.AppArgs.ErrorLog) {
-		apachelogger.SetErrLog(server)
-	}
-	setupSignals(server)
-
-	if 0 < len(APPNAME.AppArgs.CertKey) && (0 < len(APPNAME.AppArgs.CertPem)) {
-		// start the HTTP to HTTPS redirector:
-		go http.ListenAndServe(APPNAME.AppArgs.Addr, http.HandlerFunc(redirHTTP))
-
-		// see:
-		// https://ssl-config.mozilla.org/#server=golang&version=1.14.1&config=old&guideline=5.4
-		server.TLSConfig = &tls.Config{
-			MinVersion:               tls.VersionTLS10,
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-				tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
-				tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-				tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-				tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
-				tls.TLS_RSA_WITH_RC4_128_SHA,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256, // #nosec G402
-			},
-		} // #nosec G402
-		// server.TLSNextProto = make(map[string]func(*http.Server, *tls.Conn, http.Handler))
-
-		s := fmt.Sprintf("%s listening HTTPS at: %s", Me, APPNAME.AppArgs.Addr)
-		log.Println(s)
-		apachelogger.Log("APPNAME/main", s)
-		exit(fmt.Sprintf("%s: %v", Me,
-			server.ListenAndServeTLS(APPNAME.AppArgs.CertPem, APPNAME.AppArgs.CertKey)))
-		return
-	}
-
-	s := fmt.Sprintf("%s listening HTTP at: %s", Me, APPNAME.AppArgs.Addr)
-	log.Println(s)
-	apachelogger.Log("APPNAME/main", s)
-	exit(fmt.Sprintf("%s: %v", Me, server.ListenAndServe()))
-} // main()
-
-/* _EoF_ */
+func (r *Resolver) autoRefresh(rate time.Duration) {
+  for {
+    time.Sleep(rate)
+    r.Refresh()
+  }
+}
--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module github.com/mwat56/dnscache
 
 go 1.23
-
-require (
-	github.com/NYTimes/gziphandler v1.1.1
-	github.com/mwat56/apachelogger v1.7.0
-	github.com/mwat56/errorhandler v1.1.11
-)
