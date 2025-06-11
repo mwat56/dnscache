98b6446f528e81623a089ec2f5a14d0e9a4d692c
Matthias Watermann <support@mwat.de>
2025-06-11 16:18:24 +0200

    + provide `New()` constructor function to create a `ICacheList` instance; * update doc-comments and unit-tests;

---
+++ cache/newInterface.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2025  M.Watermann, 10247 Berlin, Germany
+
+	    All rights reserved
+	EMail : <support@mwat.de>
+*/
+package cache
+
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+type (
+	// `TCacheType` is the type of cache list to create.
+	TCacheType int8
+)
+
+const (
+	// `CacheTypeMap` is a map-based cache list.
+	CacheTypeMap = TCacheType(-1)
+
+	// `CacheTypeTrie` is a trie-based cache list.
+	CacheTypeTrie = TCacheType(1)
+
+	// `DefaultCacheSize` is the initial size of the cache list.
+	DefaultCacheSize = 1 << 10 // 1024
+)
+
+// ---------------------------------------------------------------------------
+// `ICacheList` constructor:
+
+// `New()` returns a new IP address cache list.
+//
+// There are currently two types of cache lists available:
+//   - `CacheTypeMap`: A map-based cache list,
+//   - `CacheTypeTrie`: A trie-based cache list.
+//
+// The cache type is determined by the `aType` parameter.
+// The trie-based cache is the default.
+//
+// The `aSize` argument is relevant only for the map-based cache list.
+// If the value is zero, the default size (`1024`) is used.
+//
+// Parameters:
+//   - `aType`: Type of cache to create.
+//   - `aSize`: Initial size of the map-based cache.
+//
+// Returns:
+//   - `ICacheList`: A new IP address cache list.
+func New(aType TCacheType, aSize uint) ICacheList {
+	if CacheTypeMap == aType {
+		return newMap(aSize)
+	}
+
+	return newTrie()
+} // New()
+
+/* _EoF_ */
---
+++ cache/newInterface_test.go
@@ -0,0 +1,109 @@
+/*
+Copyright © 2025  M.Watermann, 10247 Berlin, Germany
+
+	    All rights reserved
+	EMail : <support@mwat.de>
+*/
+package cache
+
+import (
+	"testing"
+)
+
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+func Test_New(t *testing.T) {
+	tests := []struct {
+		name  string
+		cType TCacheType
+		cSize uint
+		want  ICacheList
+	}{
+		{
+			name:  "01 - nil",
+			cType: CacheTypeMap,
+			cSize: 0,
+			want:  &tCacheList{},
+		},
+		{
+			name:  "02 - empty",
+			cType: CacheTypeMap,
+			cSize: 0,
+			want:  &tCacheList{},
+		},
+		{
+			name:  "03 - default size",
+			cType: CacheTypeMap,
+			cSize: DefaultCacheSize,
+			want:  &tCacheList{},
+		},
+		{
+			name:  "04 - custom size",
+			cType: CacheTypeMap,
+			cSize: 128,
+			want:  &tCacheList{},
+		},
+		{
+			name:  "05 - trie",
+			cType: CacheTypeTrie,
+			cSize: 0,
+			want: &tTrieList{
+				tRoot: tRoot{
+					node: newNode(),
+				},
+			},
+		},
+		{
+			name:  "06 - trie with size",
+			cType: CacheTypeTrie,
+			cSize: 128,
+			want: &tTrieList{
+				tRoot: tRoot{
+					node: newNode(),
+				},
+			},
+		},
+
+		// TODO: Add test cases.
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := New(tc.cType, tc.cSize)
+
+			if nil == got {
+				if nil != tc.want {
+					t.Error("New() = nil, want non-nil")
+				}
+				return
+			}
+			if nil == tc.want {
+				t.Errorf("New() =\n%v\nwant 'nil'",
+					got)
+				return
+			}
+
+			gotMap, Gok := got.(*tCacheList)
+			wantMap, Wok := tc.want.(*tCacheList)
+			if Gok && Wok {
+				if !wantMap.Equal(gotMap) {
+					t.Errorf("New() =\n%q\nwant\n%q",
+						gotMap, wantMap)
+				}
+				return
+			}
+
+			gotTrie, Gok := got.(*tTrieList)
+			wantTrie, Wok := tc.want.(*tTrieList)
+			if Gok && Wok {
+				if !wantTrie.Equal(gotTrie) {
+					t.Errorf("New() =\n%q\nwant\n%q",
+						gotTrie, wantTrie)
+				}
+				return
+			}
+		})
+	}
+} // Test_New()
+
+/* _EoF_ */
